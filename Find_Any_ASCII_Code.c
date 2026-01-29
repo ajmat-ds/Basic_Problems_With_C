18c221d6073d1e23e2995c8e878b20a91984ebed
ajmat-ds <160166086+ajmat-ds@users.noreply.github.com>
2026-01-29 07:52:24 +0530

    Add files via upload

---
+++ Find_Any_ASCII_Code.c
@@ -0,0 +1,19 @@
+// Find ASCII Code of the characters +,-,*,/,A
+
+#include <stdio.h>
+int main(){
+    char x ='+';
+    printf("ASCII code of + is %d \n",x);
+
+    char y = '-';
+    printf("ASCII code of - is %d  \n",y);
+
+    char z = '*';
+    printf("ASCII code of * is %d \n",z);
+
+    char a = '/';
+    printf("ASCII code of / is %d \n",a);
+
+    char b ='A';
+    printf("ASCII code of A is %d \n",b);
+}
